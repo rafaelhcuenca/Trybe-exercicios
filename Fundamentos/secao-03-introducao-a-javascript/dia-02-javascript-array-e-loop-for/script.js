d7eb000c720974854971b7e274a88cd9fd93ad3d
Rafael Cuenca <faelhc_@hotmail.com>
2023-05-04 23:22:00 -0300

    Adicione contato

--- Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -1,4 +1,15 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = 1;
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = 1;
 
-console.log(menuServices);
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = 2;
+
+// console.log(indexOfPortfolio);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push('Contato');
+
+console.log(menu);
