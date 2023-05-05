74f8744b10b92e23565e35be5c86eb0d2d784447
Rafael Cuenca <faelhc_@hotmail.com>
2023-05-04 23:26:36 -0300

    Coreções

--- Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -1,15 +1,15 @@
 // let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-// let menuServices = 1;
+// let menuServices = menu[1];
 
 // console.log(menuServices);
 
-// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-// let indexOfPortfolio = 2;
-
-// console.log(indexOfPortfolio);
-
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
 
-menu.push('Contato');
+console.log(indexOfPortfolio);
 
-console.log(menu);
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// menu.push('Contato');
+
+// console.log(menu);
